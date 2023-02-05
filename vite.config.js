f7c7c61c515cd43181d26114b93eafdae93b51fb
blue.crona <blue.crona@gmail.com>
2023-02-05 12:38:55 +0000

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/frontendmentor-tip-calculator-app/",
+  plugins: [react(), ViteFaviconsPlugin("./src/assets/logo.png")],
+});
